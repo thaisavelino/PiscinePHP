34cc6038c4a280bf48b2c7c214a9040d3928c2d1
yo <yo@gmail.com>
2019-03-26 15:01:19 +0100

    ex 04 05

--- Day01/ex03/main.php
+++ Day01/ex03/main.php
@@ -1,5 +1,4 @@
 #!/usr/bin/php
 <?PHP
     include("ft_split.php");
-	print_r(ft_split("Hello    World AAA"));
-?>
+	print_r(ft_split("Hello    World AAA")); ?>
---
+++ Day01/ex04/aff_param.php
@@ -0,0 +1,6 @@
+#!/usr/bin/php
+<?php
+    unset($argv[0]);
+    foreach($argv as $v)
+        echo $v."\n";
+?>
