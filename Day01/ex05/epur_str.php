57f1e57ea179d43b93050a3054f0e6f6cfa6f4c2
yo <yo@gmail.com>
2019-03-26 15:54:39 +0100

    [ex05] ok test with tabs

--- Day01/ex05/epur_str.php
+++ Day01/ex05/epur_str.php
@@ -2,7 +2,7 @@
 <?php
     if ($argc == 2) 
     {
-        $array = array_filter(explode(' ', $argv[1]));
+        $array = array_filter(explode(' ', $argv[1]);
         $res = implode(" ", $array);    
         echo "$res\n";
     }
