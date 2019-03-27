c57c029f8c175dff36ea17110937fc9f480890b9
yo <yo@gmail.com>
2019-03-27 15:08:17 +0100

    ex08 ok

--- Day01/ex08/ft_is_sort.php
+++ Day01/ex08/ft_is_sort.php
@@ -3,15 +3,5 @@ function ft_is_sort($array)
 {
     $array_sorted = $array;
     sort($array_sorted);
-  /*  foreach($array as $v)
-    {
-        print($v);
-    }
-    print("\n");
-    foreach($array_sorted as $v)
-    {
-        print($v);
-    }
-    print("\n");*/
     return ($array_sorted == $array);
 }
