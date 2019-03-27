2696b8c28fdc42dff769bcc68ab622a84d5dc303
yo <yo@gmail.com>
2019-03-27 17:17:06 +0100

    ok

--- Day01/ex12/search_it!.php
+++ Day01/ex12/search_it!.php
@@ -7,17 +7,15 @@
             $dots = strpos($argv[$i], ":");
 			if ($dots) {
 				$key2 = substr($argv[$i], 0, $dots);
-				echo "|$argv[$i]|\n key2: $key2\n";
 				$value = substr($argv[$i], $dots+1);
-				echo "value $value\n";
 				$str[$key2] = $value;
-				echo "str[key2] = $str[$key2] \n";
 			}
         }
-        #search key at str and print it
+		#search key at str and print it
 		if ($str[$key]) {
-			echo "str0 - $str[0]\n answer ";
 			echo "$str[$key]\n";
+		} else if ($str[$key] == "0") {
+			echo "0\n";
 		}
 	}
 ?>
--- Day02/ex00/another_world.php
+++ Day02/ex00/another_world.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-	if ($argc > 1)
+	if ($argc >= 2)
 	{
 		$data = trim(preg_replace('#\s+#', ' ', $argv[1]));
 		echo $data."\n";
