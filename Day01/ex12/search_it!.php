3ea1b1a620493f6c980d6333da7f613f0a000023
yo <yo@gmail.com>
2019-03-27 13:52:08 +0100

    ok 12

--- Day01/ex12/search_it!.php
+++ Day01/ex12/search_it!.php
@@ -2,11 +2,9 @@
 <?php
 	if ($argc >= 3) {
         $key = $argv[1];
-        echo "key $key\n";
 		for ($i = 2; $i < $argc; $i++) {
             //spread key and value from : 
             $dots = strpos($argv[$i], ":");
-            echo "dots $dots\n";
 			if ($dots) {
 				$key2 = substr($argv[$i], 0, $dots);
                 $value = substr($argv[$i], $dots+1);
