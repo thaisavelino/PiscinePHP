dc1f15091f747d7f5773d70676f54ec73a5bdec8
yo <yo@gmail.com>
2019-03-27 09:46:30 +0100

    take out the parameters check, because we not supposed to do this

--- Day01/ex10/do_op.php
+++ Day01/ex10/do_op.php
@@ -5,9 +5,6 @@ if ($argc == 4) {
     $a = trim($argv[1]);
     $o = trim($argv[2]);
     $b = trim($argv[3]);
-}
-// Check right params and run
-if (is_numeric($a) && is_numeric($b) && ((($o == "+") || ($o == "-") ||  ($o == "/") || ($o == "%") || ($o == "*"))) ) {
     switch ($o) {
         case '+':
             print($a+$b);
