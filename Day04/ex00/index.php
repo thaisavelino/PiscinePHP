73c261a4627db0bba48e52a5182a5858c723743f
thaisavelino <tavelino@student.42.fr>
2019-03-29 19:45:09 +0100

    need design

--- Day04/ex00/index.php
+++ Day04/ex00/index.php
@@ -1,16 +1,33 @@
 <?php 
 
-//session_start();
+session_start();
 
-if( $_GET["login"] && $_GET["passwd"]){
+if ($_GET && isset($_GET['submit']) && $_GET['submit'] === "OK") {
+    if (isset($_GET['login']) && !empty($_GET['login']))
+        $_SESSION['login'] = $_GET['login'];
+    if (isset($_GET['passwd']) && !empty($_GET['passwd']))
+        $_SESSION['passwd'] = $_GET['passwd'];
     echo "Welcome ". $_GET['login']. "<br />";
-    echo "Your password is ". $_GET['passwd'];
+    echo "Your password is ". $_GET['passwd']."<br />";
+   // print_r($_SESSION);
 }
- /*   $_SESSION['user'] = [
-        'email' => $_GET['email']
-    ];*/
+
+
+
+// Display
+/*
+$login = '';
+$password = '';
+if (isset($_SESSION) && $_SESSION['login'])
+    $login = $_SESSION['login'];
+if (isset($_SESSION) && $_SESSION['passwd'])
+    $password = $_SESSION['passwd'];
+    */
 ?>
 <html>
+<head>
+    <title>Session</title>
+</head>
 <body>
     <form action="<?php $_PHP_SELF ?>" method="get">
         Username: <input type="text" placeholder="" name="login" /> <br />
