433569bd27c46fe6f01aba70f062ca306a009add
thaisavelino <tavelino@student.42.fr>
2019-04-08 14:16:38 +0200

    comments on ex05

--- Day07/ex05/NightsWatch.class.php
+++ Day07/ex05/NightsWatch.class.php
@@ -10,7 +10,7 @@ Class NightsWatch implements IFighter
     {
         foreach ($this->warrior as $new)
         {
-            if (method_exists(get_class($new), 'fight'))
+            if (method_exists(get_class($new), 'fight')) //get class return the name of the class // method exists check if exists.. rs
                 $new->fight();
         }
     }
